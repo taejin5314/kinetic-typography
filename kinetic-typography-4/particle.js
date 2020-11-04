16ae0f36fec1900f248ea7bec237f5e626b011a2
taejin <taejin5314@gmail.com>
2020-11-03 22:21:31 -0500

    kinetic typography 4

--- kinetic-typography-4/particle.js
+++ kinetic-typography-4/particle.js
@@ -6,6 +6,47 @@ export class Particle {
         this.color = color;
         this.maxRadius = Math.random() * (50 - 20) + 20;
 
-        this.
+        this.savedX = pos.x;
+        this.savedY = pos.y;
+        this.x = pos.x;
+        this.y = pos.y;
+
+        this.progress = 0;
+        this.radius = 2;
+        this.vr = 0;
+        this.vx = 0;
+        this.vy = 0;
+
+        this.fps = 30;
+        this.fpsTime = 1000 / this.fps;
+    }
+
+    draw(ctx) {
+        if (process < 100) {
+            this.vr += (this.maxRadius - this.radius) / this.fpsTime;
+            this.vr *= MOVE_SPEED;
+        } else {
+            this.vr -= 2;
+        }
+
+        this.progress += 1;
+
+        this.radius += this.vr;
+
+        this.x += (this.savedX - this.x) * MOVE_SPEED;
+        this.y += (this.savedY - this.y) * MOVE_SPEED;
+
+        this.vx *= FRICTION;
+        this.vy *= FRICTION;
+
+        this.x += this.vx;
+        this.y += this.vy;
+
+        if (this.radius > 1) {
+            ctx.beginPath();
+            ctx.fillStyle = this.color;
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+            ctx.fill();
+        }
     }
 }
---
+++ kinetic-typography-4/visual.js
@@ -0,0 +1,61 @@
+import { Text } from './text.js';
+import { Particle } from './particle.js';
+
+export class Visual {
+    constructor() {
+        this.text = new Text();
+
+        this.particles = [];
+
+        this.mouse = {
+            x: 0,
+            y: 0,
+            radius: 100
+        };
+
+        document.addEventListener('pointermove', this.onMove.bind(this), false);
+    }
+
+    show(stageWidth, stageHeight) {
+        this.pos = this.text.setText('W', 20, stageWidth, stageHeight);
+        this.posTotal = this.pos.length - 1;
+    }
+
+    animate(ctx) {
+        if (!this.pos) {
+            return;
+        }
+
+        for (let i = 0; i < 10; i++) {
+            const myPos = this.pos[(Math.random() * this.posTotal) | 0];
+            this.particles.push(new Particle(myPost, this.getColor()));
+        }
+
+        for (let i = 0; i < this.particles.length; i++) {
+            const item = this.particles[i];
+            if (item.radius <= 1) {
+                this.particles.splice(i, 1);
+            }
+
+            const dx = this.mouse.x - item.x;
+            const dy = this.mouse.y - item.y;
+            const dist = Math.sqrt(dx * dx + dy * dy);
+            const minDist = item.radius + this.mouse.radius;
+
+            if (dist < minDist) {
+                item.prgress += 100;
+            }
+
+            item.draw(ctx);
+        }
+    }
+
+    getColor() {
+        return '#ff0000'
+    }
+
+    onMove(e) {
+        this.mouse.x = e.clientX;
+        this.mouse.y = e.clientY;
+    }
+}
